4b33f78c8d52f6bef5b85d566b90bb43494df338
yuki-js <me@aoki.app>
2024-10-11 12:47:29 +0900

    add security context

--- src/main/java/org/acme/ProtectedResource.java
+++ src/main/java/org/acme/ProtectedResource.java
@@ -9,7 +9,9 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 
 @Path("/protected")
 public class ProtectedResource {
@@ -20,9 +22,10 @@ public class ProtectedResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @RolesAllowed("**")
-    public String getProtectedResource() {
+    public String getProtectedResource(@Context SecurityContext securityContext) {
         return "{\"hello\": \"world\", \"nonce\": \"" + idToken.getClaim("nonce") + "\", \"raw_token\": \""
-                + idToken.getRawToken() + "\"}";
+                + idToken.getRawToken() + "\", \"principal\": \"" + securityContext.getUserPrincipal().getName()
+                + "\"}";
 
     }
 }
